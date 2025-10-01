dcdda297c976849b851a88a09365b8d5f908a986
sz3333 <135136616+sz3333@users.noreply.github.com>
2025-10-01 11:33:40 +0300

    Update help.py

--- hikka/modules/help.py
+++ hikka/modules/help.py
@@ -42,7 +42,7 @@ class Help(loader.Module):
             ),
             loader.ConfigValue(
                 "desc_icon",
-                "<emoji document_id=5188377234380954537>🪐</emoji>",
+                "<emoji document_id=5454359873212923789>☃️</emoji>",
                 lambda: "Desc emoji",
             ),
         )
@@ -137,7 +137,7 @@ class Help(loader.Module):
         )
 
         reply = "{} <b>{}</b>:".format(
-            "<emoji document_id=5134452506935427991>🪐</emoji>",
+            "<emoji document_id=5454359873212923789>☃️</emoji>",
             _name,
             ""
         )
@@ -343,7 +343,7 @@ class Help(loader.Module):
                 (
                     utils.get_platform_emoji()
                     if self._client.hikka_me.premium and CUSTOM_EMOJIS
-                    else "🪐"
+                    else "☃️"
                 )
             ),
         )
